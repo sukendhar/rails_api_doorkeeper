d1eaa8ddb96628c049444085c0cf6ef82a564bb6
maisaengineering <contact@maisasolutions.com>
2014-04-16 15:39:42 +0530

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsApiDoorkeeper::Application.config.secret_key_base = '2f773e2a340eaff7d938dbee58d168dee8571e3ccfd141e444834e7255c0b82bd4f30566ca2d4c844f6e8e6a984b06a25480349fcf0095bd2a3c2b84e2f0ccbc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RailsApiDoorkeeper::Application.config.session_store :cookie_store, key: '_rails_api_doorkeeper_session'
