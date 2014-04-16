4ec3645e759ddc9e0bda0fd0c359831347e4df75
sukendhar <sukendhar555@gmail.com>
2014-04-16 16:15:46 +0530

    first commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User
   include Mongoid::Document
+  include Mongoid::Timestamps
   rolify
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
